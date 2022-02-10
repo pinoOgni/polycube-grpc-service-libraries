0a460c639e7b69b1d2f53c215fe2027a484e4608
Giuseppe Ognibene <ognibenegiuseppe8@gmail.com>
2022-02-10 19:32:30 +0100

    update example hellogord2 in go with tests, added dataplane code of classic helloworld

--- examples/go/pcn-helloworld2/helloworld2.go
+++ examples/go/pcn-helloworld2/helloworld2.go
@@ -4,6 +4,7 @@ import (
 	pb "github.com/pinoOgni/polycube-grpc-service-libraries/polycube-grpc-go" // pb stands for polycube
 	"time"
 	"fmt"
+	"io/ioutil"
 	"unsafe"
 )
 
@@ -23,32 +24,84 @@ type rt_v struct {
 func main() {
 	pb.CreatePolycubeClient()
 	time.Sleep(1 * time.Second)
-	//pb.Subscribe("helloworld")
+	
+	// TABLE GET AND TABLE SET
+	pb.Subscribe("helloworldgo")
+	time.Sleep(4*time.Second)
+	ingress := GetDataplaneCode("Helloworld2_dp_ingress.c")
+	egress := GetDataplaneCode("Helloworld2_dp_egress.c")
+	// pino: TODO, ora come ora è lo sviluppatore che deve configurare il proprio conf perché la libreria è generica
+    conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
+	pb.CreateCube("h1",ingress,egress,conf)
+	time.Sleep(4*time.Second)
+
+	var key uint32 = 0
+	var value uint8
+	r_get := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
+	fmt.Println(r_get)
+	time.Sleep(4*time.Second)
+	value = 2 // slowpath
+	r_set := pb.TableSet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),value,unsafe.Sizeof(value))
+	fmt.Println(r_set)  // should be true
+	time.Sleep(4*time.Second)
+	r_get2 := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
+	fmt.Println(r_get2)
+	time.Sleep(4*time.Second)
+	fmt.Println("the end")
+	time.Sleep(100*time.Second)
+}
+
+
+
+func GetDataplaneCode(filename string) string {
+	b, err := ioutil.ReadFile(filename)
+    if err != nil {
+        fmt.Print(err)
+    }
+	return string(b)
+}
+
+
+
+// TESTING EXAMPLES
+/*
+	// SUBSCRIBE AND UNSUBSCRIBE METHODS
+	
+	pb.Subscribe("helloworldgo")
 	fmt.Println("ciao")
 	time.Sleep(4*time.Second)
-	// go pb.ReadTheStream()
-	// pb.DestroyCube("h1")
-	// var key uint32 = 0
-	// var value uint8 
-	// r := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
-	// fmt.Println(r)
+	pb.Unsubscribe()
+
+
+	// CREATE CUBE AND DESTROY CUBE
+
+	ingress := GetDataplaneCode("Helloworld2_dp_ingress.c")
+	egress := GetDataplaneCode("Helloworld2_dp_egress.c")
+	// pino: TODO, ora come ora è lo sviluppatore che deve configurare il proprio conf perché la libreria è generica
+    conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
+	pb.CreateCube("h1",ingress,egress,conf)
+	time.Sleep(4*time.Second)
+	pb.DestroyCube("h1")
 	
-	key_rt_k := rt_k{
-		netmask_len: 24,
-		network: 196618,
-	}
-	var value_rt_v rt_v
-	r1 := pb.TableGet("r1","routing_table",pb.CubeInfo_INGRESS,key_rt_k,unsafe.Sizeof(key_rt_k),unsafe.Sizeof(value_rt_v))
-	fmt.Println(r1)
+	// TABLE GET AND TABLE SET
+	pb.Subscribe("helloworldgo")
+	ingress := GetDataplaneCode("Helloworld2_dp_ingress.c")
+	egress := GetDataplaneCode("Helloworld2_dp_egress.c")
+	// pino: TODO, ora come ora è lo sviluppatore che deve configurare il proprio conf perché la libreria è generica
+    conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
+	pb.CreateCube("h1",ingress,egress,conf)
+	time.Sleep(4*time.Second)
 
-	/*
+	var key uint32 = 0
+	var value uint8
+	r := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
+	fmt.Println(r)
 
-    rt_k key{
-        .netmask_len = 24, 
-        .network = 196618, // x0a\x00\x03\x00 is in big endian, 00, 03, 00, 0a is in little endian
-                          // 0a ==> 10, 00 ==> 0, 03 ==> 0*16^5 + 3*16^4 ==> 196608 , 00 ==> 0 ====> 10+196608 = 196618
-    };
-	*/
+	value = 1 // forward
+	r := pb.TablesET("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),value,unsafe.Sizeof(value))
+	fmt.Println(r)  // should be true
 
+	r := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
+	fmt.Println(r)
 
-}
+*/
