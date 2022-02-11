fa4ced77e26fcf285a923dbb6f4a8607fc3812d5
Giuseppe Ognibene <ognibenegiuseppe8@gmail.com>
2022-02-11 17:00:24 +0100

    added some tests for new methods and update doc

--- examples/go/pcn-helloworld2/helloworld2.go
+++ examples/go/pcn-helloworld2/helloworld2.go
@@ -5,7 +5,8 @@ import (
 	"time"
 	"fmt"
 	"io/ioutil"
-	"unsafe"
+	"sync"
+	// "unsafe"
 )
 
 
@@ -25,30 +26,28 @@ func main() {
 	pb.CreatePolycubeClient()
 	time.Sleep(1 * time.Second)
 	
-	// TABLE GET AND TABLE SET
+
+
+
 	pb.Subscribe("helloworldgo")
-	time.Sleep(4*time.Second)
 	ingress := GetDataplaneCode("Helloworld2_dp_ingress.c")
 	egress := GetDataplaneCode("Helloworld2_dp_egress.c")
+	time.Sleep(4*time.Second)
 	// pino: TODO, ora come ora è lo sviluppatore che deve configurare il proprio conf perché la libreria è generica
-    conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
+	conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
 	pb.CreateCube("h1",ingress,egress,conf)
 	time.Sleep(4*time.Second)
 
-	var key uint32 = 0
-	var value uint8
-	r_get := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
-	fmt.Println(r_get)
-	time.Sleep(4*time.Second)
-	value = 2 // slowpath
-	r_set := pb.TableSet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),value,unsafe.Sizeof(value))
-	fmt.Println(r_set)  // should be true
-	time.Sleep(4*time.Second)
-	r_get2 := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
-	fmt.Println(r_get2)
-	time.Sleep(4*time.Second)
+
+	// pino: TODO se lo metto prima e dopo faccio qualcosa mi da errore e crasha
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go pb.ReadTheStreamGoRoutine(&wg)
+	wg.Wait()
+
+	
 	fmt.Println("the end")
-	time.Sleep(100*time.Second)
+	// time.Sleep(100*time.Second)
 }
 
 
@@ -63,45 +62,3 @@ func GetDataplaneCode(filename string) string {
 
 
 
-// TESTING EXAMPLES
-/*
-	// SUBSCRIBE AND UNSUBSCRIBE METHODS
-	
-	pb.Subscribe("helloworldgo")
-	fmt.Println("ciao")
-	time.Sleep(4*time.Second)
-	pb.Unsubscribe()
-
-
-	// CREATE CUBE AND DESTROY CUBE
-
-	ingress := GetDataplaneCode("Helloworld2_dp_ingress.c")
-	egress := GetDataplaneCode("Helloworld2_dp_egress.c")
-	// pino: TODO, ora come ora è lo sviluppatore che deve configurare il proprio conf perché la libreria è generica
-    conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
-	pb.CreateCube("h1",ingress,egress,conf)
-	time.Sleep(4*time.Second)
-	pb.DestroyCube("h1")
-	
-	// TABLE GET AND TABLE SET
-	pb.Subscribe("helloworldgo")
-	ingress := GetDataplaneCode("Helloworld2_dp_ingress.c")
-	egress := GetDataplaneCode("Helloworld2_dp_egress.c")
-	// pino: TODO, ora come ora è lo sviluppatore che deve configurare il proprio conf perché la libreria è generica
-    conf := `{"name":"h1","type":"TC","loglevel":"INFO","shadow":false,"span":false,"action":"drop"}`
-	pb.CreateCube("h1",ingress,egress,conf)
-	time.Sleep(4*time.Second)
-
-	var key uint32 = 0
-	var value uint8
-	r := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
-	fmt.Println(r)
-
-	value = 1 // forward
-	r := pb.TablesET("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),value,unsafe.Sizeof(value))
-	fmt.Println(r)  // should be true
-
-	r := pb.TableGet("h1","action_map",pb.CubeInfo_INGRESS,key,unsafe.Sizeof(key),unsafe.Sizeof(value))
-	fmt.Println(r)
-
-*/
